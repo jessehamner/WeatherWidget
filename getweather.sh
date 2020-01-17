8ca59f9753e7ad962173d621d67c8f1b4fe4a434
Jesse Hamner <jesse.hamner@bloomreach.com>
2020-01-17 14:37:19 -0600

    Fixed a problem with the animated gif (timestamps are a problem when you're comparing images that would have been identical)

--- getweather.sh
+++ getweather.sh
@@ -33,17 +33,34 @@ do
   fi
 done
 
-cp ${dir}/wow.gif ${dir}/wow_0.gif
-sha0=$(shasum /tmp/wow_0.gif | awk {'print $1'} | tr '\n' ' ' | sed 's/ $//g')
+# Get the MD5 hash of the backup image (no time stamp):
+sha0=$(shasum /tmp/wow_00.gif | awk {'print $1'} | tr '\n' ' ' | sed 's/ $//g')
 ${convert_binary} -composite ${dir}/current_image.gif   ${dir}/current_warnings.gif  ${dir}/weather.gif
 ${convert_binary} -composite ${dir}/weather.gif   ${dir}/bkg2.gif  ${dir}/wow.gif
+
+# Copy the potentially new image (no time stamp) to a backup image:
+cp ${dir}/wow.gif ${dir}/wow_00.gif
+
+# Make an MD5 hash of the potentially new image:
 sha1=$(shasum /tmp/wow.gif | awk {'print $1'} | tr '\n' ' ' | sed 's/ $//g')
 
+# Check the two hashes against each other (backup to possibly-new):
 if [[ "${sha0}" == "${sha1}" ]]; then
   echo "Radar image has not changed."
-   exit
+  exit
 fi
 
+# Keep a clean copy for comparison next time
+# cp ${dir}/wow.gif ${dir}/wow_00.gif
+
+# Timestamp the image with the system time:
+${convert_binary} -font Courier -pointsize 18 -strokewidth 0.5 -stroke white \
+  -draw "fill white text 530,50 '$(date +%H:%M)'" ${dir}/wow.gif ${dir}/wow.gif
+
+# Copy the new time-stamped image to frame 0:
+cp ${dir}/wow.gif ${dir}/wow_0.gif
+
+# Make an animated GIF from the timestamped images.
 imagecount=20
 animate=" -delay 100 -dispose Background "
 for i in $(seq ${imagecount} -1 0)
@@ -57,7 +74,6 @@ for i in $(seq ${imagecount} -1 0)
   done
 animate="${animate} -loop 0 ${dir}/animation.gif"
 
-echo "animate command: ${animate}"
-
+# echo "animate command: ${convert_binary} ${animate}"
 ${convert_binary} ${animate}
 
