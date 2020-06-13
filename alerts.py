37ebab0f05359b250a13b2c42f712e98dc463a3a
Jesse Hamner <jhamner@gmail.com>
2020-06-12 19:40:05 -0500

    Heavy refactoring of Alerts class. Added subclass for alert data storage and classification.

--- alerts.py
+++ alerts.py
@@ -10,9 +10,87 @@ import os
 import re
 import logging
 import requests
-import json
 from bs4 import BeautifulSoup
 import weather_functions as wf
+import hwo
+
+
+class EventDict(object):
+  """
+  Store one event in a common format.
+  """
+
+  def __init__(self, entry, data):
+    """
+    Instantiate the event object and create an empty dictionary.
+    """
+    self.entry = entry
+    self.sum_str = '{sev} {event_type}, {st_date} - {en_date}\nSummary: {summary}\n\n'
+    self.eventdict = {'event_type': '',
+                      'startdate': '',
+                      'enddate': '',
+                      'severity': '',
+                      'certainty': '',
+                      'summary': '',
+                      'event_id': '',
+                      'warning_summary': ''
+                     }
+    self.data = data
+
+  def populate(self):
+    """
+    Filter and fill up the eventdict instance.
+    """
+    summary = self.entry.find('summary').text
+    summary = re.sub(r'\*', '\n', summary)
+
+    event_type = self.entry.find('event', text=True)
+    if event_type:
+      print('Event_type info: {0}'.format(event_type.text))
+      event_type = event_type.text
+    else:
+      print('No cap:event tags in entry?')
+
+    startdate = self.entry.find('effective', text=True)
+    if startdate:
+      # print('Startdate: {0}'.format(startdate.text))
+      startdate = startdate.text
+
+    # category = entry.find('category').string
+    self.eventdict = {'event_type': event_type,
+                      'startdate': startdate,
+                      'enddate': self.entry.find('expires').string,
+                      'severity': self.entry.find('severity').text,
+                      'certainty': self.entry.find('certainty').text,
+                      'summary': summary,
+                      'event_id': self.entry.find('id').text,
+                      'alert_type': self.classify_alert(event_type)
+                     }
+    self.eventdict['warning_summary'] = self.sum_str.format(event_type=event_type,
+                                                            st_date=startdate,
+                                                            en_date=self.eventdict['enddate'],
+                                                            sev=self.eventdict['severity'],
+                                                            summary=summary)
+    return self.eventdict
+
+
+  def classify_alert(self, event_type):
+    """
+    Some warnings are more urgent than others. This method classifies the
+    warnings according to immediate threat to life and property, but I am
+    not a lawyer and this is really just the way I perceive the usefulness
+    of seeing alerts on a dashboard.
+    """
+
+    if event_type.lower() in self.data['defaults']['watches']:
+      return 'watch'
+    elif event_type.lower() in self.data['defaults']['warnings']:
+      return 'warn'
+    elif event_type.lower() in self.data['defaults']['alerts']:
+      return 'alert'
+
+    print('Unable to classify {0}'.format(event_type))
+    return 'alert'
 
 
 class Alerts(object):
@@ -23,7 +101,7 @@ class Alerts(object):
   def __init__(self,
                data='',
                outputfile='current_hwo.txt',
-               outputalertsfile = 'alerts_text.txt'):
+               outputalertsfile='alerts_text.txt'):
     """
     Instantiate the Alerts object and set up common variables.
     """
@@ -32,12 +110,14 @@ class Alerts(object):
     self.hwo_text = ''
     self.outputfile = outputfile
     self.outputalertsfile = outputalertsfile
-    self.alert_dict = dict()
-
-    self.alerts = dict(hwo=dict(spotter=[], dayone=[], daystwothroughseven=[], today_text=''),
-                       alerts=dict(issues=[]),
-                       watch=dict(issues=[]),
-                       warn=dict(issues=[])
+    self.alerts = dict(hwo=dict(),
+                       flags=dict(has_watches=False,
+                                  has_warnings=False,
+                                  has_alerts=False,
+                                  has_spotter=False),
+                       alert=[],
+                       watch=[],
+                       warn=[]
                       )
 
 
@@ -51,158 +131,57 @@ class Alerts(object):
 
     """
     nodata = 'No data'
-    self.get_hwo()
-    self.split_hwo()
-    
+    hwo_dict = hwo.HWO(self.data)
+    hwo_dict.get_hwo()
+    hwo_dict.split_hwo()
+    self.alerts['hwo'] = hwo_dict.hwodict
+
     textfilepath = os.path.join(self.data['output_dir'], 'today_hwo.txt')
     if not self.alerts['hwo']['today_text']:
       self.alerts['hwo']['today_text'] = ('Hazardous Weather Outlook:\n ' + nodata)
-    self.write_text(filepath=textfilepath, some_text=self.alerts['hwo']['today_text'])
-   
-    jsonfilepath = os.path.join(self.data['output_dir'], 'hwo.json')
+    wf.write_text(filepath=textfilepath, some_text=self.alerts['hwo']['today_text'])
+
     if self.alerts['hwo'] is None:
-      self.alerts['hwo'] = dict(today=[nodata, nodata], spotter=[nodata,nodata],
-                           daystwothroughseven=[nodata, nodata])
-    self.write_json(filepath=jsonfilepath, some_dict=self.alerts['hwo'])
+      self.alerts['hwo'] = dict(today=[nodata, nodata], spotter=[nodata, nodata],
+                                daystwothroughseven=[nodata, nodata])
+    wf.write_json(outputdir=self.data['output_dir'],
+                  filename='hwo.json',
+                  some_dict=self.alerts['hwo'])
 
     print('Getting alerts for the following counties: {0}.'.format(self.data['alert_counties']))
-    self.alert_dict = self.get_current_alerts()
+    self.get_current_alerts()
+
+    self.set_flags()
+
+
     alertpath = os.path.join(self.data['output_dir'], self.outputalertsfile)
-    if not self.alert_dict:
+    if not self.alerts:
       try:
         if os.path.exists(alertpath):
           os.remove(alertpath)
-      except OSError:
-        pass
+      except OSError as exc:
+        print('OS Error: {0}'.format(exc))
 
-    self.write_json(os.path.join(self.data['output_dir'], 'alerts.json'), self.alert_dict)
-    self.write_dict(filepath = alertpath, some_dict = self.alert_dict) 
-
-    return True
-
-
-  def write_text(self, filepath, some_text):
-    """
-    Write a text string out to a file.
-    """
-    with open(filepath, 'w') as text_file:
-      print('writing text to {0}'.format(filepath))
-      text_file.write(some_text)
-    text_file.close()
-    return True
-
-
-  def write_dict(self, filepath, some_dict):
-    """
-    Write out a dict to a text file.
-    """
-    with open(filepath, 'w') as current_alerts:
-      for key, value in some_dict.iteritems():
-        print('Key for this alert entry: {0}'.format(key))
-        current_alerts.write('{0}\n'.format(value['warning_summary']))
+    wf.write_json(some_dict=self.alerts,
+                  filename='alerts.json',
+                  outputdir=self.data['output_dir'])
+    wf.write_dict(filepath=alertpath, some_dict=self.alerts)
 
     return True
 
 
-  def write_json(self, filepath, some_dict):
+  def set_flags(self):
     """
-    Write out a JSON file of a given dict.
+    Go through the alerts and HWO and see what booleans should be set.
     """
-    with open(filepath, 'w') as out_obj:
-      print('writing json to {0}'.format(filepath))
-      try:
-        out_obj.write(json.dumps(some_dict))
-        print('raw dict: {0}'.format(some_dict))
-        return True
-      except Exception as e:
-        print('Ugh: {0}'.format(e))
-        return False
-
-
-  def get_hwo(self):
-    """
-    Get the HTML-only Hazardous Weather Outlook. The raw text of this statement
-    is available inside
-
-    <body>
-      <div id="local"> <div id="localcontent">
-      <pre class="glossaryProduct">
-      (Text is here)
-      </pre>
-    """
-    params_dict = {'site': self.data['hwo_site'],
-                   'issuedby': self.data['nws_abbr'],
-                   'product': 'HWO',
-                   'format': 'txt',
-                   'version': 1,
-                   'glossary': 0
-                  }
-
-    response = requests.get(self.defaults['hwo_url'],
-                            params=params_dict,
-                            verify=False,
-                            timeout=10)
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    pres = soup.body.find_all('pre')
-    for pretag in pres:
-      self.hwo_text = pretag.get_text()
-      if len(self.hwo_text) > 200:
-
-        cur = open(os.path.join(self.data['output_dir'], self.outputfile), 'w')
-        cur.write(self.hwo_text)
-        cur.close()
-        return self.hwo_text
-
-    return None
-
-
-  def split_hwo(self):
-    """
-    Pull out today's hazardous weather outlook and spotter activation notice.
-    Return a slightly more compact text block of the two paragraphs.
-
-    """
-    bodytext = self.hwo_text
-    returntext = ''
-    # print('Raw body text of HWO: \n{0}'.format(bodytext))
-
-    dayone = re.search(r'(\.DAY ONE.*?)(\.DAYS TWO THROUGH SEVEN.*?)', bodytext, re.DOTALL)
-    if dayone:
-      hwotext = re.sub(r'\n\n$', '', dayone.group(1))
-      hwotext = re.sub(r'\.{1,}DAY ONE[\.]{1,}', '', hwotext)
-      first_sentence = re.search(r'^(.*)\.', hwotext).group(1)
-      # print('First sentence: {0}'.format(first_sentence))
-      hwotext = re.sub('\n', ' ', hwotext)
-      first_info = re.sub(first_sentence, '', hwotext)
-      first_info = re.sub('^\s*\.*', '', first_info)
-      self.alerts['hwo']['dayone'] = [first_sentence.strip(), first_info.strip()]
-      
-
-    daytwo = re.search('DAYS TWO THROUGH SEVEN(.*)SPOTTER', bodytext, re.DOTALL).group(1)
-    if daytwo:
-      print('DayTwo: {0}'.format(daytwo))
-      daytwo = re.sub(r'\n{1,}', ' ', daytwo)
-      daytwo = re.sub(r'\.{3,}\s*', ' ', daytwo)
-      first_sentence = re.search(r'^(.*?)\.', daytwo).group(1)
-      # print('First sentence: {0}'.format(first_sentence))
-      second_info = re.sub(first_sentence, '', daytwo)
-      second_info = re.sub('^\s*\.*', '', second_info)
-      self.alerts['hwo']['daystwothroughseven'] = [first_sentence.strip(),
-                                                   second_info.strip()]
-
-    spotter = re.search(r'(\.SPOTTER INFORMATION STATEMENT.*?)(\s*\$\$)',
-                        bodytext, re.DOTALL)
-    if spotter:
-      spottext = re.sub(r'\n\n$', '', spotter.group(1))
-      spottext = re.sub(r'\.{1,}SPOTTER INFORMATION STATEMENT[\.]{1,}',
-                         '', spottext)
-      spottext = re.sub('\n', ' ', spottext)
-      self.alerts['hwo']['spotter'] = ['Spotter Information Statement',
-                                       spottext.strip()]
-
-    if spottext:
-      self.alerts['hwo']['today_text'] = '{0}{1}\n\n'.format(self.alerts['hwo']['dayone'][1], spottext)
+    if self.alerts['warn']:
+      self.alerts['flags']['has_warnings'] = True
+    if self.alerts['watch']:
+      self.alerts['flags']['has_watches'] = True
+    if self.alerts['warn']:
+      self.alerts['flags']['has_warnings'] = True
+    if self.alerts['warn']:
+      self.alerts['flags']['has_warnings'] = True
     return True
 
 
@@ -239,8 +218,8 @@ class Alerts(object):
       response = requests.get(self.data['defaults']['alerts_url'],
                               params=county_params_dict,
                               verify=False, timeout=10)
-    except Exception as e:
-      print('Exception when requesting current alerts: {0}'.format(e))
+    except Exception as exc:
+      print('Exception when requesting current alerts: {0}'.format(exc))
       return None
 
     if response.status_code == 200 and response.headers['Content-Type'] == 'text/xml':
@@ -250,53 +229,21 @@ class Alerts(object):
       print('ERROR: Response from NWS alerts server is: {0}'.format(response.status_code))
       return None
 
-    sum_str = '{sev} {event_type}, {st_date} - {en_date}\nSummary: {summary}\n\n'
     for entry in entries:
       print('Now checking alerts xml.')
       warning_county = self.is_county_relevant(entry,
                                                tagname='areaDesc')
       if warning_county:
-        # print('Found warning for {0} county.'.format(warning_county))
-        # print('Text of this warning entry is: {0}'.format(entry.prettify()))
+        print('Found warning for {0} county.'.format(warning_county))
+        print('Text of this warning entry is: {0}'.format(entry.prettify()))
         event_id = entry.find('id').text
-        summary = entry.find('summary').text
-        summary = re.sub(r'\*', '\n', summary)
+        print('event id appears to be: {0}'.format(event_id))
 
-        event_type = entry.find('event', text=True)
-        if event_type:
-          print('Event_type info: {0}'.format(event_type.text))
-          event_type = event_type.text
-        else:
-          print('No cap:event tags in entry?')
+        edt = EventDict(entry, self.data)
+        edt.populate()
+        self.alerts[edt.eventdict['alert_type']] = edt.eventdict
 
-        startdate = entry.find('effective', text=True)
-        if startdate:
-          # print('Startdate: {0}'.format(startdate.text))
-          startdate = startdate.text
-
-        enddate = entry.find('expires').string
-        # category = entry.find('category').string
-        severity = entry.find('severity').text
-        certainty = entry.find('certainty').text
-        warning_summary = sum_str.format(event_type=event_type,
-                                         st_date=startdate,
-                                         en_date=enddate,
-                                         sev=severity,
-                                         summary=summary)
-        # print('Warning summary:\n{0}'.format(warning_summary))
-
-        eventdict = {'event_type': event_type,
-                     'startdate': startdate,
-                     'enddate': enddate,
-                     'severity': severity,
-                     'certainty': certainty,
-                     'summary': summary,
-                     'event_id': event_id,
-                     'warning_summary': warning_summary
-                    }
-        self.alert_dict[event_id] = eventdict
-
-    return self.alert_dict
+    return self.alerts
 
 
   def is_county_relevant(self, xml_entry, tagname='areaDesc'):
@@ -313,8 +260,8 @@ class Alerts(object):
       clist = entry_counties.text.split(';')
       if not clist:
         return None
-    except AttributeError as e:
-      print('Attribute Error: {0}. Returning None.'.format(e))
+    except AttributeError as exc:
+      print('Attribute Error: {0}. Returning None.'.format(exc))
       return None
 
     for county in clist:
@@ -323,39 +270,3 @@ class Alerts(object):
         return county.strip()
 
     return None
-
-
-  def classify_alerts(self):
-    """
-    Some warnings are more urgent than others. This method classifies the
-    warnings according to immediate threat to life and property, but I am
-    not a lawyer and this is really just the way I perceive the usefulness
-    of seeing alerts on a dashboard.
-    """
-    oh_shit = ['Blizzard Warning', 'Ice Storm Warning', 'High Wind Warning',
-               'Severe Thunderstorm Warning', 'Tornado Warning',
-               'Extreme Wind Warning', 'Gale Warning', 'Hurricane Force Wind Warning',
-               'Flash Flood Warning', 'Flood Warning', 'River Flood Warning',
-               'Tropical Storm Warning', 'Hurricane Warning', 'Coastal Flood Warning']
-    be_aware = ['Winter Storm Watch', 'Winter Weather Advisory', 'Freeze Watch',
-                'Freeze Warning', 'Frost Advisory', 'Wind Chill Advisory',
-                'Wind Chill Warning', 'Red Flag Warning', 'Dense Fog Advisory',
-                'High Wind Watch', 'Wind Advisory', 'Severe Thunderstorm Watch',
-                'Tornado Watch', 'Small Craft Advisory', 'Storm Warning',
-                'Special Marine Warning', 'Coastal Flood Watch', 'Flash Flood Watch',
-                'Flood Watch', 'River Flood Watch', 'Excessive Heat Watch',
-                'Excessive Heat Warning', 'Heat Advisory', 'Tropical Storm Watch',
-                'Hurricane Watch']
-
-    for alert in self.alert_dict:
-      if alert['event_type'] in oh_shit:
-        self.alerts['warn'].append(alert)
-      elif alert['event_type'] in be_aware:
-        self.alerts['watch'].append(alert)
-      else:
-        print('Unable to classify {0}'.format(alert['event_type']))
-        self.alerts['alerts'].append(alert)
-        continue
-
-    return self.alerts
-
