b65abb98d67351413d59796dc68b1f42a75f1ad4
Jesse Hamner <jhamner@gmail.com>
2020-06-18 15:34:09 -0500

    Linting. Fixed some scope errors.

--- forecast.py
+++ forecast.py
@@ -7,11 +7,11 @@ from __future__ import print_function
 import os
 import re
 import logging
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 from weather_functions import write_json
-from weathersvg import assign_icon
+import weathersvg as wsvg
 
 
 class DayForecast(object):
@@ -19,10 +19,65 @@ class DayForecast(object):
   Store one day's forecast and do useful stuff with the information.
   """
 
-  def __init__(self):
+  def __init__(self, output_dir, idx, parsed_xml, icon_dict):
     self.fcd = dict(high=None, low=None, precip_morning=0, precip_evening=0,
                     shortcast='No forecast', forecast_string='',
-                    day='Noneday', date=None, icon='na.svg')
+                    day='Noneday', date=None, icon='na.svg', idx=idx)
+    self.idx = idx
+    self.parsed_xml = parsed_xml
+    self.icon_dict = icon_dict
+    self.output_dir = output_dir
+    self.offset = ''
+
+
+  def make_forecast_icons(self):
+    """
+    Write out SVG icons of the day's temps and precip chances.
+    """
+    filelabel = 'today_{fctype}_plus_{day}.svg'
+    wsvg.high_low_svg(self.fcd['high'],
+                      self.fcd['low'],
+                      filelabel.format(fctype='temp', day=self.idx),
+                      outputdir=self.output_dir
+                     )
+    wsvg.precip_chance_svg(int(self.fcd['precip_morning']),
+                           int(self.fcd['precip_evening']),
+                           filename=filelabel.format(fctype='precip', day=self.idx),
+                           outputdir=self.output_dir
+                          )
+
+    return True
+
+
+  def populate_day_dict(self):
+    """
+    Populate a single day's forecast dict.
+    """
+    params = self.parsed_xml.find('parameters')
+    temps = params.find_all("temperature")
+    for temp in range(0, len(temps)):
+      if temps[temp].attrs['type'] == "maximum":
+        self.fcd['high'] = temps[temp].find_all("value")[self.idx].text
+        continue
+      self.fcd['low'] = temps[temp].find_all("value")[self.idx].text
+
+    precip = params.find('probability-of-precipitation')
+    self.fcd['precip_morning'] = precip.find_all("value")[self.idx].text
+    self.fcd['precip_evening'] = precip.find_all("value")[self.idx + 1].text
+    self.fcd['shortcast'] = params.find_all("weather-conditions")[self.idx].attrs['weather-summary']
+
+    parseable_time = self.parsed_xml.find('time-layout').find_all("start-valid-time")[self.idx].text
+    parseable_time1, self.offset = re.sub(r'([+|-])(\d{2}):(\d{2})$',
+                                          ' \\1\\2\\3',
+                                          parseable_time).split()
+    valid_time = datetime.strptime(parseable_time1, "%Y-%m-%dT%H:%M:%S")
+    self.fcd['day'] = valid_time.strftime("%a")
+    self.fcd['date'] = valid_time.strftime("%Y-%m-%d")
+    self.fcd['icon'] = wsvg.assign_icon(self.fcd['shortcast'],
+                                        self.icon_dict)
+    self.make_forecast_icons()
+    # print(today.fcd)
+    return self.fcd
 
 
 class Forecast(object):
@@ -37,6 +92,7 @@ class Forecast(object):
     self.forecast = []
     self.afd = []
     self.offset = ''
+    self.parsed_xml = ''
 
 
   def get_afd(self, fmt='txt'):
@@ -67,7 +123,7 @@ class Forecast(object):
 
     try:
       response = requests.get(url, params=args, verify=False, timeout=10)
-    except requests.exceptions.ReadTimeout as e:
+    except requests.exceptions.ReadTimeout:
       print('Request timed out. Returning -None-')
       return None
 
@@ -106,56 +162,39 @@ class Forecast(object):
     if not fmt or fmt is None:
       fmt = ['24', 'hourly']
     time_format = " ".join(fmt)
-    payload = {'lon': self.data['lon'], 'lat': self.data['lat'],
-               'format': time_format, 'numDays': self.data['defaults']['forecast_days']}
-    retval = requests.get(url=self.data['defaults']['forecast_url'], params=payload,
-                          verify=False, timeout=10)
+    payload = {'lon': self.data['lon'],
+               'lat': self.data['lat'],
+               'format': time_format,
+               'numDays': self.data['defaults']['forecast_days']
+              }
+    retval = requests.get(url=self.data['defaults']['forecast_url'],
+                          params=payload,
+                          verify=False,
+                          timeout=10
+                         )
     if retval.status_code == 200:
       self.data['forecast_xml'] = retval.text
       print('Returned HTTP response code: {0}'.format(retval))
       self.parsed_xml = BeautifulSoup(self.data['forecast_xml'], 'xml')
       return retval
-    print('Unable to successfully retrieve forecast -- server returned {0}'.format(retval))
+    print('Cannot retrieve forecast -- server returned {0}'.format(retval))
     return None
-
-
-  def populate_day_dict(self, idx):
-    """
-
-    """
-    params = self.parsed_xml.find('parameters')
-    today = DayForecast()
-    temps = params.find_all("temperature")
-    for x in range(0, len(temps)):
-      if temps[x].attrs['type'] == "maximum":
-        today.fcd['high'] = temps[x].find_all("value")[idx].text
-        continue
-      today.fcd['low'] = temps[x].find_all("value")[idx].text
-
-    precip = params.find('probability-of-precipitation')
-    today.fcd['precip_morning'] = precip.find_all("value")[idx].text
-    today.fcd['precip_evening'] = precip.find_all("value")[idx + 1].text
-    today.fcd['shortcast'] = params.find_all("weather-conditions")[idx].attrs['weather-summary']
-
-    parseable_time = self.parsed_xml.find('time-layout').find_all("start-valid-time")[idx].text
-    parseable_time1, self.offset = re.sub(r'([+|-])(\d{2}):(\d{2})$',
-                                          ' \\1\\2\\3',
-                                          parseable_time).split()
-    valid_time = datetime.strptime(parseable_time1, "%Y-%m-%dT%H:%M:%S")
-    today.fcd['day'] = valid_time.strftime("%a")
-    today.fcd['date'] = valid_time.strftime("%Y-%m-%d")
-    today.fcd['icon'] = assign_icon(today.fcd['shortcast'], self.data['defaults']['icon_match'])
-    # print(today.fcd)
-    return today.fcd
 
 
   def parse_forecast(self):
     """
     Use bs4 to parse the XML returned from the AWS forecast request.
-
     """
-    for i in range(0, self.data['defaults']['forecast_days']):
-      self.forecast.append(self.populate_day_dict(i))
+    for idx in range(0, self.data['defaults']['forecast_days']):
+
+      today = DayForecast(output_dir=self.data['output_dir'],
+                          idx=idx,
+                          parsed_xml=self.parsed_xml,
+                          icon_dict=self.data['defaults']['icon_match']
+                         )
+      today.populate_day_dict()
+      today.make_forecast_icons()
+      self.forecast.append(today.fcd)
 
     return self.forecast
 
@@ -180,54 +219,19 @@ class Forecast(object):
     Write out a nicely formatted text file using the retrieved and summarized
     forecast information.
     """
-    fc = self.forecast
+    fcst = self.forecast
     with open(os.path.join(outputdir, filename), 'w') as fc_text:
       fc_text.write('  Forecast:\n')
       fc_text.write('-'*50 + '\n')
       for i in range(0, 3):
-        precip_str = '{0:3d}% / {1:3d}%'.format(int(fc[i]['precip_morning']),
-                                                int(fc[i]['precip_evening']))
-        line = '{0}  {1:3d}  {2:3d}  {3:10s}  {4}'.format(fc[i]['day'],
-                                                          int(fc[i]['high']),
-                                                          int(fc[i]['low']),
+        precip_str = '{0:3d}% / {1:3d}%'.format(int(fcst[i]['precip_morning']),
+                                                int(fcst[i]['precip_evening']))
+        line = '{0}  {1:3d}  {2:3d}  {3:10s}  {4}'.format(fcst[i]['day'],
+                                                          int(fcst[i]['high']),
+                                                          int(fcst[i]['low']),
                                                           precip_str,
-                                                          fc[i]['shortcast'],
+                                                          fcst[i]['shortcast'],
                                                          )
         fc_text.write('{0}\n'.format(line))
 
       return True
-
-
-  def make_forecast_icons(self, fc_dict, outputdir='/tmp/'):
-    """
-    Write out SVG icons of the next three days of temps and precip chances.
-    """
-    filelabel = 'today_{fctype}_plus_{day}.svg'
-    for i in range(0, 3):
-      high_low_svg(fc_dict['highs'][i], fc_dict['lows'][i],
-                   filelabel.format(fctype='temp', day=i),
-                   outputdir=outputdir
-                  )
-      precip_chance_svg(int(fc_dict['pcp_pct'][i][0]),
-                        int(fc_dict['pcp_pct'][i][1]),
-                        filename=filelabel.format(fctype='precip', day=i),
-                        outputdir=outputdir
-                       )
-
-    return True
-
-
-  def get_dates(bs_object, hourly='24hourly'):
-    """
-    Get the day of the week from the next seven days' forecast.
-    """
-    dates = []
-    time_layouts = bs_object.find('data').find_all('time-layout')
-    for i in time_layouts:
-      if i['summarization'] == hourly:
-        for j in i.find_all('start-valid-time'):
-          reftime = re.sub(r'-0\d{1}:00$', ' CST', j.string)
-          t_time = datetime.datetime.strptime(reftime, "%Y-%m-%dT%H:%M:%S %Z")
-          dates.append(datetime.datetime.strftime(t_time, "%a"))
-
-    return dates
